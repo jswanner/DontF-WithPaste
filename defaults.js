46a9b3865bc5b555f6aed2f648d60da09a076d29
Nasser <nrh.990@gmail.com>
2017-01-18 16:10:58 +0300

    Disable on *.live.com
    OneNote was fucking with paste and DFWP was fucking with it back.
    
    Also, my first pull request. yay

--- defaults.js
+++ defaults.js
@@ -1,6 +1,7 @@
 const exclude = `https?://.*?\.facebook\.com/.*
 https?://.*?.google.com/.*
 https?://.*?.?github.com/.*
+https?://.*?.live.com/.*
 https?://imgur.com/.*`;
 
 window.defaultValues = {
