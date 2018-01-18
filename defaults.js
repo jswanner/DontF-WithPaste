be467233b015a0d1bf5bcd8b464aec3600225569
Martin Eskdale Moen <martinmoen@gmail.com>
2018-01-18 16:38:55 +0000

    Added slack to exclusion list
    Was cauing problems with Slack formatting.
    Related issue https://github.com/jswanner/DontFuckWithPaste/issues/45

--- defaults.js
+++ defaults.js
@@ -1,7 +1,8 @@
 const exclude = `https?://.*?\.facebook\.com/.*
 https?://.*?.google.com/.*
 https?://.*?.?github.com/.*
-https?://imgur.com/.*`;
+https?://imgur.com/.*
+https?://.*?\.slack.com/.*`;
 
 window.defaultValues = {
   exclude,
